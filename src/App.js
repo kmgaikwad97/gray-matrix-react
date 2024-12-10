a83974ebdd85f6480f276635f075f6528d01b74b
kmgaikwad97 <kmgaikwad97@gmail.com>
2024-12-10 14:17:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Easy Task</h1>
+      <h1>Easy Task 1</h1>
       {
         colorBox.map((color)=>(
           <div className='h-[150px] w-[150px] border border-black' style={{backgroundColor:color}}>
